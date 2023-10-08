0a43f44d089a6077213759534b817524ddb49975
Bruno Ricardo Cavalli <cavallibruno27@gmail.com>
2023-10-08 20:54:36 -0300

    Correção Controller

--- Controllers/ServicoController.cs
+++ Controllers/ServicoController.cs
@@ -1,92 +1,45 @@
-namespace AgendayServico.Controllers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using AgendayMKll.Models;
-
-public class Agenda
+using Microsoft.AspNetCore.Mvc;
+using Agenday.Models;
+namespace Agenday.Controllers
 {
-    private List<Usuario> usuarios = new List<Usuario>();
-
-    public Agenda(List<Usuario> usuarios)
+    [Route("api/servico")]
+    [ApiController]
+    public class ServicoController : Controller
     {
-        this.usuarios = usuarios;
-    }
+        private List<Servico> _servicos = new List<Servico>();
 
-    private List<Servico> servicos = new List<Servico>();
-    private List<Cliente> clientes = new List<Cliente>();
-
-    public void AdicionarUsuario(Usuario usuario)
-    {
-        usuario.Id = usuarios.Count + 1;
-        usuarios.Add(usuario);
-    }
-
-    public void AdicionarServico(Servico servico)
-    {
-        servico.Id = servicos.Count + 1;
-        servicos.Add(servico);
-    }
-
-    public void AdicionarCliente(Cliente cliente)
-    {
-        cliente.Id = clientes.Count + 1;
-        clientes.Add(cliente);
-    }
-
-    public void ListarUsuarios()
-    {
-        foreach (var usuario in usuarios)
+        // Endpoint para listar todos os serviços
+        [HttpGet]
+        public IActionResult ListarServicos()
         {
-            Console.WriteLine(value: $"ID: {usuario.Id}, Nome: {usuario.Nome}, Email: {usuario.Email}");
+            return Ok(_servicos);
+        }
+
+        // Endpoint para cadastrar um novo serviço
+        [HttpPost]
+        public IActionResult CadastrarServico([FromBody] Servico novoServico)
+        {
+            if (novoServico == null)
+            {
+                return BadRequest("Dados do serviço inválidos");
+            }
+
+            novoServico.ServicoID = Guid.NewGuid(); // Gere um ID único para o serviço
+            _servicos.Add(novoServico);
+
+            return CreatedAtAction(nameof(ListarServicos), novoServico);
         }
     }
 
-    public void ListarServicos()
+    public class Servico
     {
-        foreach (var servico in servicos)
-        {
-            Console.WriteLine($"ID: {servico.Id}, Nome: {servico.Nome}, Preço: {GetPreco(servico):C}");
-        }
-    }
-
-    private static object GetPreco(Servico servico)
-    {
-        return servico.Preco;
-    }
-
-    public void ListarClientes()
-    {
-        foreach (var cliente in clientes)
-        {
-            Console.WriteLine(value: $"ID: {cliente.Id}, Nome: {cliente.Nome}, Telefone: {cliente.Telefone}");
-        }
-    }
-
-    public void RemoverUsuario(int id)
-    {
-        var usuario = usuarios.FirstOrDefault(u => u.Id == id);
-        if (usuario != null)
-        {
-            usuarios.Remove(usuario);
-        }
-    }
-
-    public void RemoverServico(int id)
-    {
-        var servico = servicos.FirstOrDefault(s => s.Id == id);
-        if (servico != null)
-        {
-            servicos.Remove(servico);
-        }
-    }
-
-    public void RemoverCliente(int id)
-    {
-        var cliente = clientes.FirstOrDefault(c => c.Id == id);
-        if (cliente != null)
-        {
-            clientes.Remove(cliente);
-        }
+        public Guid ServicoID { get; set; }
+        public string Nome { get; set; }
+        public string Cliente { get; set; }
+        public string Usuario { get; set; }
+        public decimal Preco { get; set; }
     }
 }
